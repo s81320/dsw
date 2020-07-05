d5dd57e09ea8d4dff407c61a3b3bfcb3488ea783
Silke Meiner <s81320@beuth-hochschule.de>
2020-07-05 21:23:52 +0200

    collect links

---
+++ get-links-tsp.py
@@ -0,0 +1,31 @@
+import requests
+import urllib.request
+import time
+import pandas as pd
+
+from bs4 import BeautifulSoup
+
+url= 'http://www.tagesspiegel.de'
+response = requests.get(url)
+print(response)
+soup = BeautifulSoup(response.text , "html.parser")
+
+all_a = soup.findAll('a')
+
+
+link_list = []
+for one_a in all_a:
+    if ( one_a['href'][-5:]== '.html' ):
+            if(one_a['href'][0:4]=='http') :
+          	    link_list.append(one_a['href'])  
+            else:
+          	    link_list.append('https://www.tagesspiegel.de' + one_a['href'])
+
+link_list = list(set(link_list))
+
+print('wrote links to file: ' , len(link_list))
+
+with open("links-tsp.txt" , "a+") as file:
+	for link in link_list:
+		file.write(link + '\n')
+
