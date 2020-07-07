b1b3fb074696b5a1a5f02d5aa05b0dc78992df92
Silke Meiner <s81320@beuth-hochschule.de>
2020-07-07 21:44:48 +0200

    rmoved coments

--- data-acquisition/get-content-tsp.py
+++ data-acquisition/get-content-tsp.py
@@ -3,12 +3,6 @@ import time
 import sys , os
 import json
 
-#article = Article('https://www.nytimes.com/2020/07/06/us/Epidemiologists-coronavirus-protests-quarantine.html?action=click&module=Top%20Stories&pgtype=Homepage')
-#article.download()
-#article.parse()
-#print(article.authors)
-#print(article.text)
-
 # first filename
 i=0
 
@@ -43,23 +37,7 @@ with open("links-tsp.txt" , "r") as link_file :
 
 		print("wrote " + str(i))
 
-		#with open(filename, 'w') as output_file:
-		#	output_file.write('paper: tagesspiegel ' + '\n')
-		#	output_file.write('source: ' + link + '\n')
-		#	if(len(article.authors)==0):
-		#		output_file.write("author: none\n")
-		#	else:
-		#		output_file.write("author: " )
-		#		for author in article.authors:
-		#			output_file.write(author + ', ')
-		#	output_file.write("\n")
-		#	output_file.write('title: ' + article.title + '\n')
-		#	output_file.write("publication_date: " + str(article.publish_date )+ '\n')
-		#	output_file.write('articleid: ' + str(article.meta_data['articleid'])+ '\n')
-		#	output_file.write('image: ' + article.top_image + '\n')
-		#	output_file.write('text: ' + article.text + '\n')
-		#	print("done" + str(i))
-
+		
 # keine direkte artikel id vorhanden, aber html dateien sind als nummern codiert
 #  'url': 'https://www.tagesspiegel.de/politik/kommando-zurueck-was-gegen-die-wiedereinfuehrung-der-wehrpflicht-spricht/25978222.html',
 # also einfach den dateinamen verwenden.
